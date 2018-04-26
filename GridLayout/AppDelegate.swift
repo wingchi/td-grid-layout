4b8a5bba36e25ac3fa516bce08e6550d4f6becc3
S. Wingchi Wong <stephenwong@intrepid.io>
2018-04-26 13:22:45 -0400

    Base project with collection view

---
+++ GridLayout/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  GridLayout
+//
+//  Created by wingchi on 4/23/18.
+//  Copyright © 2018 The Test and the Driven. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ GridLayout/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  GridLayout
+//
+//  Created by wingchi on 4/23/18.
+//  Copyright © 2018 The Test and the Driven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionView()
+    }
+
+    func setupCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+}
